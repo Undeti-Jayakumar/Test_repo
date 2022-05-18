88809d9b703e94e2ba656b0091e775fd8d04fb66
alpha_git_user <alpha_git_user@gmail.com>
2022-05-18 16:44:19 +0530

    my first python program

---
+++ src/python_test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import Train_test_split
+from sklearn.metrics import accuracy
+
+print("this is a python program")
